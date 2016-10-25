06e60279e1af0b15792063eee0bb66ee49d18853
shuailing <smart_an@163.com>
2016-10-25 11:10:44 +0800

    modify

--- src/com/qiandai/mypack/Hello.java
+++ src/com/qiandai/mypack/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
    public static void main(String [] args){
 	   System.out.println("hello world!");
 	   System.out.println("aaaaa");
+	   System.out.println("bbbbb");
    }
 }
